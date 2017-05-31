0c6f1812d094a8fdc1e4276a477e44512a3c8e65
ding <ding@DESKTOP-2SF0NFC>
2017-05-31 23:03:01 +0800

    添加项目文件。

---
+++ WebApplication1/BO/Health.cs
@@ -0,0 +1,33 @@
+﻿using isRock.Framework;
+using isRock.Framework.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.BO
+{
+    public class Health : BusinessLogicBase
+    {
+        /// <summary>
+        /// 計算BMI
+        /// </summary>
+        /// <param name="bodyInfo"></param>
+        /// <returns></returns>
+        public ExecuteCommandDefaultResult BMI(BodyInfo bodyInfo)
+        {
+            float height = (bodyInfo.height) / 100;
+            var bmi = bodyInfo.weight / (height * height);
+            return new ExecuteCommandDefaultResult() { Data = bmi, isSuccess = true };
+        }
+    }
+
+    /// <summary>
+    /// 個人身高體重
+    /// </summary>
+    public class BodyInfo
+    {
+        public float height { get; set; }
+        public float weight { get; set; }
+    }
+}
